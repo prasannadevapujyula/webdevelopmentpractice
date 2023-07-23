3f3679f7076eb77e5a83d8221d9ea22b8eff264d
Devapujyula Sai Lakshmi Prasanna <134317801+prasannadevapujyula@users.noreply.github.com>
2023-07-23 22:30:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const hourE1=document.querySelector(".hour")
-const minuteE1=document.querySelector(".minute")
-const secondE1=document.querySelector(".second")
+const containerE1= document.querySelector(".container");
 
-function updateClock(){
-    const currentDate = new Date();
-    setTimeout(updateClock,1000);
-    const hour=currentDate.getHours();
-    const minute=currentDate.getMinutes();
-    const second=currentDate.getSeconds();
-    const hourDeg=(hour/12)*360;
-    hourE1.style. transform = 'rotate(${hourDeg}deg)';
-    const minuteDeg=(minute/60)*360;
-    minuteE1.style. transform = 'rotate(${minuteDeg}deg)';
-    const secondDeg=(second/60)*360;
-    secondE1.style. transform = 'rotate(${secondDeg}deg)';
-}
-updateClock();
+const btnE1=document.querySelector(".btn");
 
+const popupContainerE1=document.querySelector(".popup-container");
+
+const closeIconE1=document.querySelector(".close-icon");
+
+btnE1.addEventListener("click",()=>{
+    containerE1.classList.add("active");
+    popupContainerE1.classList.remove("active");
+});
+
+closeIconE1.addEventListener("click",()=>{
+    containerE1.classList.remove("active");
+    popupContainerE1.classList.add("active");
+});
