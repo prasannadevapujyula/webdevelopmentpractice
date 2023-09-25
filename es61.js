0f1494f2d1c86087462a82d6009505e417020d59
Devapujyula Sai Lakshmi Prasanna <sailakshmiprasanna0707@gmail.com>
2023-09-25 22:24:03 +0530

    Add files via upload

---
+++ es61.js
@@ -0,0 +1,17 @@
+let add= (n1,n2)=>
+{
+    console.log(n1+n2)
+}
+let oddeven=(num)=>
+{
+    if (num%2==0)
+    {
+        console.log("even")
+    }
+    else
+    {
+        console.log("odd")
+    }
+}
+oddeven(41)
+add(4,6)
