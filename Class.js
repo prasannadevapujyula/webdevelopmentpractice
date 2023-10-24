61f6efb916de240da4071c5698a0f8380faee851
Devapujyula Sai Lakshmi Prasanna <sailakshmiprasanna0707@gmail.com>
2023-10-24 12:03:20 +0530

    Add files via upload

---
+++ Class.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import './Class.css';
+import React from "react";
+
+
+
+class Class extends Component{
+    render(){
+        return(
+            <p className="love"> this is course component</p>
+        )
+    }
+
+}
+export default Class
